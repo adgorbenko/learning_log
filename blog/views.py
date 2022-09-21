89d08ea606911a35e9d0819df9e69cb60d3a32d1
adgorbenko <114092107+adgorbenko@users.noreply.github.com>
2022-09-21 19:45:54 +0300

    Add files via upload

---
+++ blog/views.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
+
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
+
+def index(request):
+	"""Домашняя страница приложения Learning Log"""
+	return render(request, 'blog/index.html')
+
+@login_required
+def topics(request):
+	"""Показывает все темы."""
+	topics = Topic.objects.filter(owner=request.user).order_by('date_added')
+	context = {'topics': topics}
+	return render(request, 'blog/topics.html', context)
+
+@login_required
+def topic(request, topic_id):
+	"""Выводит одну тему и все её записи."""
+	topic = Topic.objects.get(id=topic_id)
+	# Проверка того, что тема принадлежит текущему пользователю.
+	if topic.owner != request.user:
+		raise Http404
+
+	entries = topic.entry_set.order_by('-date_added')
+	context = {'topic': topic, 'entries': entries}
+	return render(request, 'blog/topic.html', context)
+
+@login_required
+def new_topic(request):
+	"""Определяет новую тему."""
+	if request.method != 'POST':
+		# Данные не отправлялись; созается пустая форма.
+		form = TopicForm()
+	else:
+		# Отправлены данные POST; обработать данные.
+		form = TopicForm(data=request.POST)
+		if form.is_valid():
+			new_topic = form.save(commit=False)
+			new_topic.owner = request.user
+			new_topic.save()
+			return redirect('blog:topics')
+
+	# Вывести пустую или недействительную форму.
+	context = {'form': form}
+	return render(request, 'blog/new_topic.html', context)
+
+@login_required
+def new_entry(request, topic_id):
+	"""Добавляет новую запись по конкретной теме."""
+	topic = Topic.objects.get(id=topic_id)
+	
+	if request.method != 'POST':
+		# Данные не отправлялись; создаётся пустая форма.
+		form = EntryForm()
+	else:
+		# Отправлены данные POST; обработать данные.
+		form = EntryForm(data=request.POST)
+		if form.is_valid():
+			new_entry = form.save(commit=False)
+			new_entry.topic = topic 
+			new_entry.save()
+			return redirect('blog:topic', topic_id=topic_id)
+
+	# Вывести пустую или недействительную форму.
+	context = {'topic': topic, 'form': form}
+	return render(request, 'blog/new_entry.html', context)
+
+@login_required
+def edit_entry(request, entry_id):
+	"""Редактирует существущую запись."""
+	entry = Entry.objects.get(id=entry_id)
+	topic = entry.topic
+	if topic.owner != request.user:
+		raise Http404
+
+	if request.method != 'POST':
+		# Исходный запрос; форма заполняется данными текущей записи.
+		form = EntryForm(instance=entry)
+	else:
+		# Отправка данных POST; обработать данные.
+		form = EntryForm(instance=entry, data=request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('blog:topic', topic_id=topic.id)
+
+	context = {'entry': entry, 'topic': topic, 'form': form}
+	return render(request, 'blog/edit_entry.html', context)
+
+
+
